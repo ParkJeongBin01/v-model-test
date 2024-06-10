eb9845305607fcca11a608e37cef891ee472c605
ParkJeongBin <areoin@naver.com>
2024-06-10 21:47:03 +0900

    커밋 1

---
+++ src/components/InputName.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+    type="text"
+    :value="name"
+    @input="$emit('update:name', $event.target.value)"
+  />
+  <!--$emit()-> name이 업데이트 될 때마다 벨류가 바뀐다.-->
+</template>
+
+<script>
+export default {
+  name: 'InputName',
+  props: ['name'],
+};
+</script>
